1be7e1fd217aee6cace841d9a5f125e4b38696b2
iAMpt <trifpatricia@yahoo.com>
2023-11-17 12:56:47 +0200

    dashboard template

--- src/main/java/com/controller/HelloController.java
+++ src/main/java/com/controller/HelloController.java
@@ -1,13 +1,15 @@
 package com.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
-    @RequestMapping
-    public String helloWord() {
-        return "Dashboard";
+    @RequestMapping("/")
+    public String helloWorld(Model model) {
+        model.addAttribute("message", "Dashboard");
+        return "dashboard";
     }
 }
