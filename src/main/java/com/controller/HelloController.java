a76cf284d3aca4f4a69b54c05f6f86257ca5d36b
iAMpt <trifpatricia@yahoo.com>
2023-11-30 11:26:34 +0200

    Login Backend finish

--- src/main/java/com/controller/HelloController.java
+++ src/main/java/com/controller/HelloController.java
@@ -2,20 +2,14 @@ package com.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
 
-    @RequestMapping("/register")
-    public String register(Model model) {
-        model.addAttribute("message", "Register");
-        return "register";
-    }
-
-    @RequestMapping("/login")
-    public String login(Model model) {
-        model.addAttribute("message", "login");
+    @GetMapping("/login")
+    public String login() {
         return "login";
     }
 
