0ee6ceb1a8c4e37cc5de424723ef93f738d9a60c
iAMpt <trifpatricia@yahoo.com>
2023-12-18 10:50:32 +0200

    comanda salvata in db impreuna cu userul activ

---
+++ src/main/java/com/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/api/user")
+    public String getCurrentUserEmail() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated()) {
+            return authentication.getName();
+        }
+        return null;
+    }
+}
